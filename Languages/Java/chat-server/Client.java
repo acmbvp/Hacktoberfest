e8054c9b42597241dcc43ed3382ae0de7e7f2772
sarru1291 <sarru1291@gmail.com>
2018-10-23 00:04:25 +0530

    readme file added

---
+++ Languages/Java/chat-server/Client.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String... s) throws IOException {
+
+        Socket socket = new Socket("127.0.0.1", 5000);
+        Scanner readFromKeyboard = new Scanner(System.in);
+        PrintStream sendToConsole = new PrintStream(System.out);
+        PrintStream sendToSocket = new PrintStream(socket.getOutputStream());
+        Scanner readFromSocket = new Scanner(socket.getInputStream());
+
+        String sendingMessage = "";
+        String receivingMessage = "";
+        sendToConsole.print("          Press CTRL+C to exit !!!\n\n");
+
+        while (true) {
+            sendToConsole.print("Client>> ");
+            sendingMessage = readFromKeyboard.nextLine();
+            sendToSocket.println(sendingMessage);
+            receivingMessage = readFromSocket.nextLine();
+            sendToConsole.println("Server>> " + receivingMessage);
+        }
+        
+    }
+}
---
+++ Languages/Java/chat-server/Server.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server {
+
+    public static void main(String... arg) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(5000);
+        Socket socket = serverSocket.accept();
+        PrintStream sendToConsole = new PrintStream(System.out);
+        sendToConsole.println("Connection established!!\n");
+        Scanner readFromKeyboard = new Scanner(System.in);
+        PrintStream sendToSocket = new PrintStream(socket.getOutputStream());
+        Scanner readFromSocket = new Scanner(socket.getInputStream());
+        String sendingMessage = "";
+        String receivingMessage = "";
+        sendToConsole.print("          Press CTRL+C to exit !!!\n\n");
+       
+        while (true) {
+                receivingMessage = readFromSocket.nextLine();
+                sendToConsole.println("Client>> " + receivingMessage);
+                sendToConsole.print("Server>> ");
+                sendingMessage = readFromKeyboard.nextLine();
+                sendToSocket.println(sendingMessage);
+            }
+        
+    }
+}
