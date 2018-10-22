9cb55ab9d3ce65701d5b9fa869f8d2d8b938191f
Akanshu <37223446+akannshu@users.noreply.github.com>
2018-10-22 23:38:24 +0530

    Revert "Revert "make the changes""

---
+++ Languages/C/Untitled-1.c
@@ -0,0 +1,8 @@
+//hello world programe
+#include<stdio.h>
+int main()
+{
+    printf("Hello World");//printf function to display
+
+    return 0;//since it is a int main,it has to return soome value ,in this case it is zero 
+}
