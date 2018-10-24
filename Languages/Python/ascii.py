496211f3eb527175825286ff5d371aa07e2a65f6
Manbir <marwahmanbirsingh@gmail.com>
2018-10-24 17:56:31 +0530

    added ascii.py

---
+++ Languages/Python/ascii.py
@@ -0,0 +1,8 @@
+# Program to find the ASCII value of the given character
+# Change this value for a different result
+c = 'p'
+
+# Uncomment to take character from user
+#c = input("Enter a character: ")
+
+print("The ASCII value of '" + c + "' is",ord(c))
