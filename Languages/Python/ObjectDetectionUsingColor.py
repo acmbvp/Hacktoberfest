553428c65fa97675993e0c317da2086e8840197e
Anjali Khandelwal <38075160+anjalikhandelwal2910@users.noreply.github.com>
2018-10-27 20:57:27 +0530

    Update ObjectDetectionUsingColor.py

--- Languages/Python/ObjectDetectionUsingColor.py
+++ Languages/Python/ObjectDetectionUsingColor.py
@@ -18,11 +18,10 @@ def main():
     while ret:
         ret,frame = cap.read()
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        
+        #range of blue color
         low = np.array([100, 50, 50])
         high = np.array([140, 255, 255])
-
-        
+                        
         image_mask = cv2.inRange(hsv, low, high)
         
         output = cv2.bitwise_and(frame, frame, mask = image_mask)
