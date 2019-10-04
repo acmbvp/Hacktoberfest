2fbc70c2077cd0595989802757ae69a397bbcd9b
Declan Byrd <declan.byrd@gmail.com>
2019-10-04 12:21:21 +0100

    Added Hello World Example
    Enhancement for Issue # 43.

---
+++ Languages/Javascript/HelloWorld/HelloWorld.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Declan Byrd
+// GITHUB:@declanbyrd
+
+const firstWord = 'Hello';
+const secondWord = 'World';
+
+const message = firstWord + secondWord;
+console.log(message);
