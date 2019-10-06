2755df15e15bc0d8fdb58c15afd451f8112c3ae6
AAYUSH KHURANA <31439631+Aayushk333@users.noreply.github.com>
2019-10-06 18:18:39 +0530

    Add files via upload

---
+++ Languages/Dart/User_Location_App/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title',
+      home: GetLocationPage(),
+    );
+  }
+}
+
+class GetLocationPage extends StatefulWidget {
+  @override
+  _GetLocationPageState createState() => _GetLocationPageState();
+}
+
+class _GetLocationPageState extends State<GetLocationPage> {
+
+  var location = new Location();
+
+  Map<String, double> userLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            userLocation == null
+                ? CircularProgressIndicator()
+                : Text("Location:" +
+                    userLocation["latitude"].toString() +
+                    " " +
+                    userLocation["longitude"].toString()),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  _getLocation().then((value) {
+                    setState(() {
+                      userLocation = value;
+                    });
+                  });
+                },
+                color: Colors.blue,
+                child: Text("Get Location", style: TextStyle(color: Colors.white),),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Map<String, double>> _getLocation() async {
+    var currentLocation = <String, double>{};
+    try {
+      currentLocation = await location.getLocation();
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
+  }
+}
