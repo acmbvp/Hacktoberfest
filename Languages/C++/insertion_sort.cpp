d731ed12be9417a625f3c7ad91a7a603bf0701a3
nafees87n <56021937+nafees87n@users.noreply.github.com>
2019-10-07 12:41:54 +0530

    Added Insertion Sort.

--- Languages/C++/insertion_sort.cpp
+++ Languages/C++/insertion_sort.cpp
@@ -1,33 +1,32 @@
 #include<iostream>
  
 using namespace std;
- 
+void insort(int a[],int n)
+{
+  int i,j;
+  for(i=1;i<=n-1;i++)
+    {
+        int temp=a[i];
+        j=i-1;
+        while((temp<a[j])&&(j>=0))
+        {
+            a[j+1]=a[j];
+            j=j-1;
+        }
+        a[j+1]=temp;
+    }
+}
 int main()
 {
-    int i,j,n,temp,a[30];
+    int n,i,a[30];
     cout<<"Enter the number of elements:";
     cin>>n;
-    cout<<"\nEnter the elements\n";
- 
+    cout<<"\nEnter the elements\n"; 
     for(i=0;i<n;i++)
     {
         cin>>a[i];
     }
- 
-    for(i=1;i<=n-1;i++)
-    {
-        temp=a[i];
-        j=i-1;
- 
-        while((temp<a[j])&&(j>=0))
-        {
-            a[j+1]=a[j];    //moves element forward
-            j=j-1;
-        }
- 
-        a[j+1]=temp;    //insert element in proper place
-    }
- 
+    intsort(a,n);
     cout<<"\nSorted list is as follows\n";
     for(i=0;i<n;i++)
     {
