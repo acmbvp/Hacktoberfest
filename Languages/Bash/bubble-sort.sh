235775d33c62ad58c626226d9327da16442bce48
Lucas Schaan <schaanlucas@gmail.com>
2019-10-08 13:19:21 -0300

    Add Bubble Sort in Bash

---
+++ Languages/Bash/bubble-sort.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+array=( "$@" )
+
+function bubble-sort {
+	new_array=(${array[@]::${#array[@]}-1}) #all elements except the last one
+	for i in "${!array[@]}"; do
+		switch=false;
+		for j in "${!new_array[@]}" ; do
+			if [ "${array[j]}" -gt "${array[j+1]}" ]; then
+				aux=${array[j]}
+				array[j]=${array[j+1]}
+				array[j+1]=$aux
+				switch=true;
+			fi;
+		done;
+		if [ $switch = false ]; then 
+			break; 
+		fi;
+	done;
+}
+
+bubble-sort
+
+echo ${array[@]}
