739a0c3045711179124029bc062a3a72b5a17f78
ARPIT CHOUDHARY <choudharyarpit99@gmail.com>
2019-10-09 12:43:36 +0530

    Added 1 java file

---
+++ Languages/Java/longest_substring.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class longest_substring {
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        String str ="Hello";
+        lengthOfLongestSubstring(str);
+    }
+}
