de3b2cb21b43a9b7d4ecb36b26354030d22dd7ed
Bilal Vural <bilal_v@hotmail.com>
2019-10-12 00:40:26 +0200

    added timeout script in python, only for linux based systems

---
+++ Languages/Python/timeout.py
@@ -0,0 +1,35 @@
+class TimeoutError(Exception):
+    def __init__(self, value="Timed Out"):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+def timeout(seconds_before_timeout):
+    def decorate(f):
+        def handler(signum, frame):
+            raise TimeoutError()
+
+        def new_f(*args, **kwargs):
+            old = signal.signal(signal.SIGALRM, handler)
+            signal.alarm(seconds_before_timeout)
+            try:
+                result = f(*args, **kwargs)
+            finally:
+                signal.signal(signal.SIGALRM, old)
+                signal.alarm(0)
+            return result
+
+        new_f.__name__ = f.__name__
+        return new_f
+
+    return decorate
+
+
+@timeout(20)
+def nap():
+	import time
+	time.sleep(21)
+
+nap()
