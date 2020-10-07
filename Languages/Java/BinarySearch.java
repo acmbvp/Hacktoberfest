25837ce16a8aa7f321b238bfcdc330d3008a11bb
tarunguptaraja <the.tarun.gupta.raja@gmail.com>
2020-10-07 10:27:03 +0530

    BinearySearchAlgo

---
+++ Languages/Java/BinarySearch.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+public class BinarySearch {
+	
+	public static int binarySearchRecursive(int[] arr, int l, int r, int x) {
+		
+		
+		return -1;
+	}
+	
+	public static int binarySearchIterative(int[] arr) {
+		
+		
+		return -1;
+	}
+
+}
