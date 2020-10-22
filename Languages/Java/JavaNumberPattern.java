f2e83296e85bd6263763209acbab7374db1860da
ashwithpoojary98 <61794427+ashwithpoojary98@users.noreply.github.com>
2020-10-22 10:47:42 +0530

    JavaNumberPattern
    Patten to print the number as shown below
    
    1111
    2    2
    3    3
    4444

---
+++ Languages/Java/JavaNumberPattern.java
@@ -0,0 +1,21 @@
+public class JavaNumberPattern{
+	public static void main(String[] args){
+	for(int i=1;i<=4;i++)
+	{
+	for(int j=1;j<=4;j++)
+	{
+	if(i>=2&&j>=2&&i<=3&&j<=3)
+	{
+		System.out.print(" ");
+	}
+	else
+	{
+		System.out.print(i);
+	}
+	
+	}
+	System.out.println();
+	
+	}
+}
+}
