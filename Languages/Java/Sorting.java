84a7b71b94df1e92a33644e4a7fbf893508e7b68
Joyal Raphel <60638195+joyal007@users.noreply.github.com>
2020-10-25 14:10:52 +0530

    Add files via upload
    Added  sorting of an array

---
+++ Languages/Java/Sorting.java
@@ -0,0 +1,49 @@
+/*
+Joyal Raphel 
+CSE B S3
+ROLL :04
+*/
+import java.util.*;
+class Sorting{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int arr[]= new int[7];
+        int temp;
+        System.out.println("Enter the element of array");
+        for(int i=0;i<7;++i)
+       {
+            arr[i]=sc.nextInt();
+       }
+
+       for(int i = 0;i<7;++i)
+       {
+           for (int j=i+1;j<7;++j)
+           {
+               if(arr[i]>arr[j])
+               {
+                   temp = arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+               }
+           }
+       }
+       System.out.println("The elements of sorted array is :");
+       for(int i=0;i<7;++i)
+       {
+           System.out.print(arr[i]+" ");
+       }
+    }
+}
+/*
+enter the element of array
+44
+88
+7
+2
+3
+11
+99
+The elements of sorted array is :
+2 3 7 11 44 88 99
+*/
