549c7c5db8fbc02634ae378d81624ada6115b1f7
shivprime94 <riyuzaki9894@gmail.com>
2021-10-01 22:38:05 +0530

    Tried something new

---
+++ Languages/Javascript/test/hello.js
@@ -0,0 +1,8 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Shiv Shankar Kanaujiya
+// GITHUB:@shivprime94
+
+const first = prompt('Enter your first name')
+const last = prompt('Enter your last name')
+alert(first + last)
